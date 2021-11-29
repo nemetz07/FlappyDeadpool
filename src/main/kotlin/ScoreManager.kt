4bbae6476d6076ab55bee16346bf48485477db92
Nemetz Norbert <nemetz07@gmail.com>
2021-11-29 09:49:47 +0100

    refactored ScoreManager

--- src/main/kotlin/ScoreManager.kt
+++ src/main/kotlin/ScoreManager.kt
@@ -1,33 +1,29 @@
 import com.almasb.fxgl.dsl.getSettings
 import org.json.JSONArray
-import org.json.JSONObject
 import java.io.File
 import java.io.IOException
 import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
 
 
 class ScoreManager(private var fileName: String) {
     private val dateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
-    var scores: JSONArray
-
-    init {
-        try {
-            scores = JSONArray(File(fileName).readText())
-        } catch (e: Exception) {
-            scores = JSONArray()
-            println(this.fileName + " not found, creating empty!")
-        }
+    val scores: JSONArray = try {
+        JSONArray(File(fileName).readText())
+    } catch (e: Exception) {
+        JSONArray()
     }
 
     fun addScore(name: String?, score: Int) {
-        val newScore = JSONObject()
-        newScore.put("name", name)
-        newScore.put("score", score)
-        newScore.put("difficulty", getSettings().gameDifficulty.toString())
-        newScore.put("date", dateFormat.format(Date()))
-        scores.put(newScore)
+        scores.put(
+            mapOf(
+                "name" to name,
+                "score" to score,
+                "difficulty" to getSettings().gameDifficulty.toString(),
+                "date" to dateFormat.format(Date()),
+            )
+        )
     }
 
     @Throws(IOException::class)
